66f85e75c93e9058e05512acdafb9a5d711e3543
David Alvarez <davidealvarez1@gmail.com>
2020-08-20 17:15:24 -0700

    going brain dead cant think

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,20 @@
 import React from "react"
+import {Link} from "gatsby"
 import "./layout.css"
+
+const ListLink = props => (
+  <li style={{display: `inline-block`, marginRight: `1rem`}}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
 
 export default function Layout({children}){
   return(
     <div>
+      <ul style={{listStyle: `none`, float: `right`}}>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
       {children}
     </div>
   )
