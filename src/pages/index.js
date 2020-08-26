49aa33efef627dc02e41bcfe5d438d04cda51acf
David Alvarez <davidealvarez1@gmail.com>
2020-08-26 10:54:57 -0700

    moved fontawesome from index.js to layout.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from "react"
 import Header from '../components/header.js'
 import "../styles/global.css"
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fab)
+
 
 
 const IndexPage = () => {
