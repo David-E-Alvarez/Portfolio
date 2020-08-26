64aa143a1b20fa07280179ab99c567bead5bc9bc
David Alvarez <davidealvarez1@gmail.com>
2020-08-25 21:45:08 -0700

    added social links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,6 +8,12 @@ export default function Contact(){
         <Header/>
         <div id="contact_div">
             <h1>Contact Me</h1>
+            <a href="https://github.com/David-E-Alvarez">
+                <i class="fab fa-github fa-3x"></i>
+            </a>{" "}
+            <a href="https://www.linkedin.com/in/david-e-alvarez/">
+            <i class="fab fa-linkedin fa-3x"></i>
+            </a>
         </div>
         </>
     )
