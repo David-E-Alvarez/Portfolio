54513cab6807c4e8dba61fcc2459b5be966777cb
David Alvarez <davidealvarez1@gmail.com>
2020-08-26 08:16:32 -0700

    uninstalled fontawesome packages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from '../components/header.js'
 import "../styles/global.css"
 
+
 export default function Contact(){
     return(
         <>
@@ -9,10 +10,10 @@ export default function Contact(){
         <div id="contact_div">
             <h1>Contact Me</h1>
             <a href="https://github.com/David-E-Alvarez">
-                <i class="fab fa-github fa-3x"></i>
+                soon to be github icon
             </a>{" "}
             <a href="https://www.linkedin.com/in/david-e-alvarez/">
-            <i class="fab fa-linkedin fa-3x"></i>
+                <h4>soon to be linkedin icon</h4>
             </a>
         </div>
         </>
