1a951668c6b1639c994999150c16f59ff090d210
David Alvarez <davidealvarez1@gmail.com>
2020-08-26 07:44:11 -0700

    random changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ export default function About(){
         <div id="about_div">
             <h1>About Me</h1>
             <p>
-                Made in California. CSU Monterey Bay graduate. Reader. Thinker. Dreamer. Yogi. Healthy. Lover. 
+                Made in California. CSU Monterey Bay graduate. Current Lambda School student. Reader. Thinker. Dreamer. Yogi. Healthy. Lover. 
             </p>
             <h3>My Work</h3>
         </div>
