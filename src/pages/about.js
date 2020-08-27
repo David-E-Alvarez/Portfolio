ef60e88fe4091d59acb79e662e4f74a7a1ce7e0e
David Alvarez <davidealvarez1@gmail.com>
2020-08-27 12:48:35 -0700

    moved font awes imports to each component

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,9 @@ import React from "react"
 import Header from '../components/header.js'
 import "../styles/global.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(fab)
 
 export default function About(){
     return(     
