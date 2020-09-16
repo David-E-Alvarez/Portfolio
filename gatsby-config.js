977273fe55643a86a654128f2d4d0f400e3bbece
David Alvarez <davidealvarez1@gmail.com>
2020-09-16 14:24:09 -0700

    edited gatsby config for blog plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
--- src/blog/2020-09-16-second-post/index.md
+++ src/blog/2020-09-16-second-post/index.md
@@ -3,5 +3,5 @@ path: "/second-post"
 date: "2020-09-16"
 title: "Second Post"
 tags: ['blog']
-excerpt: "Thinking about writing an article/blog about making a "nav" bar"
+excerpt: "Thinking about writing an article/blog about making a nav bar"
 ---
