c93c5b141da288b3d0214376f4c68ee2126d186c
David Alvarez <davidealvarez1@gmail.com>
2020-09-18 21:40:48 -0700

    bittersweet feelz

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,11 +2,26 @@ import React from 'react'
 import {graphql} from 'gatsby'
 
 const BlogPostTemplate = (props) => {
+    console.log("props: ", props)
+    const html = props.data.markdownRemark.html
     return (
         <div>
-            Blog Post Here
+            <div className='blogpost'
+                dangerouslySetInnerHTML={{__html:html}}
+            />
         </div>
     )
 }
+
+export const query = graphql`
+    query($pathSlug: String!){
+        markdownRemark(frontmatter: {path:{eq: $pathSlug}}){
+            html
+            frontmatter{
+                title
+            }
+        }
+    }
+`
 
 export default BlogPostTemplate
