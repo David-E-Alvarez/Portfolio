d0eb4bb8c5213cd3625404a49fb1e0457340552a
David Alvarez <davidealvarez1@gmail.com>
2020-09-21 16:36:54 -0700

    added header.js to blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import {graphql} from 'gatsby'
 import "../styles/global.css"
+import Header from '../components/header.js'
 
 const BlogPostTemplate = (props) => {
     console.log("props: ", props)
     const html = props.data.markdownRemark.html
     return (
+        <>
+        <Header/>
         <div>
             <div className='blogpost'
                 dangerouslySetInnerHTML={{__html:html}}
             />
         </div>
+        </>
     )
 }
 
